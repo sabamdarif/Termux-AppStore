b179ec77c6eeb07d8e25f46957ee813fe87c2f90
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-02-19 23:23:43 +0530

    add FreeTube

---
+++ apps/Freetube/uninstall.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+check_and_delete "${distro_path}/opt/AppImageLauncher/"
+check_and_delete "${distro_path}/usr/share/icons/hicolor/*/apps/.png"
+check_and_delete "${PREFIX}/share/applications/pd_added/.desktop"

44b4e1318c94e677d7e7d651c846532f3626b23e
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-02-25 18:53:43 +0530

    fix freetube

--- apps/Freetube/install.sh
+++ apps/Freetube/install.sh
@@ -2,13 +2,13 @@
 
 supported_arch="aarch64,arm"
 package_name="freetube"
-version=v0.23.1-beta
+version="v0.23.1-beta"
 app_type="distro"
 supported_distro="all"
 page_url="https://github.com/FreeTubeApp/FreeTube"
 run_cmd="/opt/AppImageLauncher/FreeTube/freetube --no-sandbox"
 
-cd ${TMPDIR}
+cd "${TMPDIR}"
 
 app_arch=$(uname -m)
 case "$app_arch" in
@@ -16,7 +16,8 @@ aarch64) archtype="arm64" ;;
 armv7*|arm) archtype="armv7l" ;;
 esac
 
-appimage_filename="freetube-${version#v}-${archtype}.AppImage"
+version_no_beta="$(echo "${version#v}" | sed 's/-.*$//')"
+appimage_filename="freetube-${version_no_beta}-${archtype}.AppImage"
 
 check_and_delete "${TMPDIR}/${appimage_filename} ${PREFIX}/share/applications/pd_added/freetube.desktop"
 
@@ -25,7 +26,7 @@ download_file "${page_url}/releases/download/${version}/${appimage_filename}"
 install_appimage "$appimage_filename" "FreeTube"
 
 print_success "Creating desktop entry..."
-cat <<DESKTOP_EOF | tee ${PREFIX}/share/applications/pd_added/freetube.desktop >/dev/null
+cat <<DESKTOP_EOF | tee "${PREFIX}/share/applications/pd_added/freetube.desktop" >/dev/null
 [Desktop Entry]
 Name=FreeTube
 Exec=pdrun ${run_cmd}

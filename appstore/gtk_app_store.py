4949981dd3700b7f140aa5034767d7a5d47888e2
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-03-20 11:51:50 +0530

    app store 0.5.3-beta

--- appstore/gtk_app_store.py
+++ appstore/gtk_app_store.py
@@ -446,7 +446,7 @@ class AppStoreWindow(Gtk.ApplicationWindow):
         
         # Set dialog properties
         about_dialog.set_program_name("Termux App Store")
-        about_dialog.set_version("0.5.2-beta")
+        about_dialog.set_version("0.5.3-beta")
         about_dialog.set_comments("A modern graphical package manager for Termux")
         about_dialog.set_copyright("© 2025 Termux Desktop (sabamdarif)")
         about_dialog.set_license_type(Gtk.License.GPL_3_0)

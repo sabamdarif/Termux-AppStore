711ca40842ff5092811d4ab69b18cdff3d11f9b3
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-03-21 12:27:05 +0530

    add new apps
    abiword eog lximage-qt evince mpv-x audacious deadbeef alacritty uget feathernotes weechat hexchat dosbox dosbox-x leafpad

---
+++ apps/Dosbox-x/install.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="dosbox-x"
+run_cmd="dosbox-x"
+version="termux_local_version"
+app_type="native"
+package_install_and_check "$package_name"

9241c45bf5fe8ee9c2c8fdc8987dff22c9a8c3a0
GitHub Action <github-actions[bot]@users.noreply.github.com>
2025-03-30 01:56:05 +0000

    Update app versions with latest releases from GitHub

--- apps/Brave-browser/install.sh
+++ apps/Brave-browser/install.sh
@@ -3,7 +3,7 @@
 supported_arch="aarch64"
 package_name="brave-browser"
 run_cmd="/opt/brave-browser/brave-browser --no-sandbox"
-version="v1.76.81"
+version="v1.76.82"
 app_type="distro"
 page_url="https://github.com/brave/brave-browser"
 working_dir="${distro_path}/opt"

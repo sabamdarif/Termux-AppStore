b980622bee95db9b9e35c8fb88edb328da4a245a
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-04-17 23:34:22 +0530

    fix spotube install for ubuntu

--- apps/Spotube/install.sh
+++ apps/Spotube/install.sh
@@ -12,21 +12,25 @@ supported_distro="all"
 
 # Check if a distro is selected
 if [ -z "$selected_distro" ]; then
-    print_failed "Error: No distro selected"
-    exit 1
+  print_failed "Error: No distro selected"
+  exit 1
 fi
 
-if [[ "$selected_distro" == "debian" ]] || [[ "$selected_distro" == "ubuntu" ]]; then
-distro_run "
+if [[ "$selected_distro" == "ubuntu" ]]; then
+  distro_run "
+sudo apt update -y -o Dpkg::Options::="--force-confnew" && sudo apt install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libgtk-3-0 libgbm1 libasound2 libx11-xcb1  libxcomposite1  libxdamage1 libxrandr2  libdrm2 libxcb-dri3-0 libxshmfence1
+"
+
+elif [[ "$selected_distro" == "debian" ]]; then
+  distro_run "
 sudo apt update -y -o Dpkg::Options::="--force-confnew" && sudo apt install -y libayatana-appindicator3-1 libwebkit2gtk-4.0-37 libwebkit2gtk-4.1-0 libwebkit2gtk-4.0-dev libavcodec-extra libasound2 libegl1-mesa libgl1-mesa-glx libgles2 libwayland-egl1 libx11-xcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 mpv libmpv-dev libxcb-xfixes0 pipewire pipewire-audio-client-libraries libgtk-3-0 libgdk-pixbuf2.0-0 libwayland-client0 libwayland-egl1 libwayland-cursor0
 "
 
 elif [[ "$selected_distro" == "fedora" ]]; then
-distro_run "
+  distro_run "
 sudo dnf install -y libayatana-appindicator webkit2gtk3 webkit2gtk4.0 ffmpeg alsa-lib mesa-libEGL mesa-libGL mesa-libGLES wayland libX11-xcb libXcomposite libXcursor libXdamage libXext libXfixes libXi libXrandr libXrender mpv mpv-libs libxcb pipewire pipewire-libs pipewire-alsa pipewire-pulseaudio gtk3 gdk-pixbuf2 wayland-devel
 "
 fi
-
 
 distro_run "
 check_and_delete '/opt/spotube'
@@ -54,4 +58,3 @@ Comment=Open source Spotify client
 MimeType=x-scheme-handler/spotube;
 Categories=Internet;
 DESKTOP_EOF
-

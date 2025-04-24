2be73a976e0d4c85916a6de5e8c36b2e514d04e5
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-04-24 15:12:58 +0530

    add neovim and nvchad

---
+++ apps/Nvchad/install.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="neovim"
+run_cmd="nvim"
+version="termux_local_version"
+app_type="native"
+package_install_and_check "$package_name git"
+check_and_backup "$HOME/.config/nvim"
+git clone https://github.com/NvChad/starter ~/.config/nvim
---
+++ apps/Nvchad/uninstall.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+package_remove_and_check "neovim"
+check_and_delete "$HOME/.config/nvim"
+check_and_delete "$HOME/.local/state/nvim"
+check_and_delete "$HOME/.local/share/nvim"

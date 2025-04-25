01acd901c1a20ff5b066027ba4236767bf96d0b6
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-04-25 11:36:43 +0530

    replace the telegram distro based package with native packge

--- apps/Telegram/uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-if [[ "$selected_distro" == "debian" ]] || [[ "$selected_distro" == "ubuntu" ]];then
-  $selected_distro remove telegram-desktop -y
-elif [[ "$selected_distro" == "fedora" ]]; then
-  $selected_distro remove telegram-desktop -y
-fi

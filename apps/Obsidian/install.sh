e1305e09ae1fa1b13de0edaa0e5c70a361a137da
GitHub Action <github-actions[bot]@users.noreply.github.com>
2025-04-27 01:58:26 +0000

    Update app versions with latest releases from GitHub

--- apps/Obsidian/install.sh
+++ apps/Obsidian/install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 supported_arch="arm64"
-version="v1.8.9"
+version="v1.8.10"
 app_type="distro"
 supported_distro="all"
 # working_dir="${distro_path}/opt/AppImageLauncher"

7881838c756bc9a8eb71e1a4fca1f2d6a636c6e5
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-05-19 09:22:49 +0530

    add lazygit and firefoxpwa

---
+++ apps/Lazygit/install.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="lazygit"
+version="termux_local_version"
+app_type="native"
+package_install_and_check "$package_name"

ce354b79abf998e023d97177310b290940c018c6
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-06-18 21:07:44 +0530

    add linuxthemestore

---
+++ apps/Linuxthemestore/install.sh
@@ -0,0 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="linuxthemestore-git"
+run_cmd="linuxthemestore"
+version="v1.0.4"
+app_type="native"
+
+app_arch=$(uname -m)
+case "$app_arch" in
+aarch64) archtype="arm64" ;;
+armv7* | arm) archtype="arm" ;;
+*) print_failed "Unsupported architectures" ;;
+esac
+
+deb_file_name="linuxthemestore-git_${version}_${archtype}.deb"
+download_file "https://github.com/sabamdarif/linuxthemestore/releases/download/${version}-termux/${deb_file_name}"
+dpkg --configure -a
+apt --fix-broken install -y
+apt install ./${deb_file_name} -y

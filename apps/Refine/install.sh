fe372257179f17e89aba06118c44907e65624740
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-07-26 12:20:28 +0530

    add refine

---
+++ apps/Refine/install.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="refine"
+run_cmd="refine"
+version="0.5.10"
+app_type="native"
+
+cd ${TMPDIR}
+
+app_arch=$(uname -m)
+case "$app_arch" in
+aarch64) archtype="aarch64" ;;
+armv7* | arm | armv8l) archtype="arm" ;;
+*) print_failed "Unsupported architectures" ;;
+esac
+
+deb_file_name=" refine_${version}_${archtype}.deb"
+check_and_delete "$deb_file_name"
+download_file "https://github.com/sabamdarif/Termux-AppStore/releases/download/files/${deb_file_name}"
+dpkg --configure -a
+apt --fix-broken install -y
+apt install ./${deb_file_name} -y

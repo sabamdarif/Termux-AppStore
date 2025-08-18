b3844d216fe020ba0bfbbe43b7d9fe52b9cd380b
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-08-18 14:47:21 +0530

    addpkg (youtube-music): v3.10.0

---
+++ apps/Youtube-music/uninstall.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+check_and_delete "${distro_path}/opt/AppImageLauncher/youtube-music"
+check_and_delete "${distro_path}/usr/share/icons/hicolor/*/apps/youtube-music.png"
+check_and_delete "${PREFIX}/share/applications/pd_added/youtube-music.desktop"

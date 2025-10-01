44f0fb279f7db72d37f32e215082926f1c66a69b
WOOD6563 <arparnasing@gmail.com>
2025-10-01 21:56:55 +0530

    WaifuDownloader

---
+++ apps/WaifuDownloader/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+supported_arch="aarch64"
+package_name="waifudownloader"
+version="0.2.9"
+app_type="native"
+
+deb_file_name="${package_name}_${version}_${supported_arch}.deb"
+download_url="https://github.com/WOOD6563/WaifuDownloader-termux/releases/download/Master/${deb_file_name}"
+
+dpkg --configure -a
+apt --fix-broken install -y
+apt install ./"$deb_file_name" -y

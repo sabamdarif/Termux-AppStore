300bc6967271e66c8751e858362de523b1eb9404
Wood6563 <arparnasing@gmail.com>
2025-10-02 12:05:15 +0530

    WaifuDownloader (#10)
    Co-authored-by: Md Arif <111168803+sabamdarif@users.noreply.github.com>

---
+++ apps/WaifuDownloader/install.sh
@@ -0,0 +1,13 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64"
+package_name="waifudownloader"
+version="0.2.9"
+app_type="native"
+
+deb_file_name="${package_name}_${version}_${supported_arch}.deb"
+download_url="https://github.com/WOOD6563/WaifuDownloader-termux/releases/download/waifudownloader/${deb_file_name}"
+
+dpkg --configure -a
+apt --fix-broken install -y
+apt install ./"$deb_file_name" -y

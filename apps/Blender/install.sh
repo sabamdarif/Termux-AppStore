e24b4f60870f9c1e3cbf2e51e7433bc4a16c57ae
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-04 19:31:08 +0530

    bump(blender): v4.5.3

--- apps/Blender/install.sh
+++ apps/Blender/install.sh
@@ -2,11 +2,11 @@
 
 supported_arch="aarch64"
 package_name="blender"
-run_cmd="blender"
-version="4.4.3"
+run_cmd="blender-4.5"
+version="4.5.3"
 app_type="native"
 
-cd ${TMPDIR}
+cd "${TMPDIR}" || exit 1
 
 app_arch=$(uname -m)
 case "$app_arch" in
@@ -14,9 +14,11 @@ aarch64) archtype="aarch64" ;;
 *) print_failed "Unsupported architectures" ;;
 esac
 
-deb_file_name="blender4_${version}_${archtype}.deb"
+deb_file_name="blender4_${version}_${archtype}.zip"
+mkdir blender
+cd blender || exit 1
 check_and_delete "$deb_file_name"
 download_file "https://github.com/sabamdarif/Termux-AppStore/releases/download/files/${deb_file_name}"
 dpkg --configure -a
 apt --fix-broken install -y
-apt install ./${deb_file_name} -y
+apt install ./*.deb -y

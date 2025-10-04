bb97637c5db49949bc193e5db3081f60e50fc460
Wood6563 <arparnasing@gmail.com>
2025-10-04 18:41:23 +0530

    Update install.sh (#11)

--- apps/WaifuDownloader/install.sh
+++ apps/WaifuDownloader/install.sh
@@ -6,9 +6,15 @@ run_cmd="waifudownloader"
 version="0.2.9"
 app_type="native"
 
-deb_file_name="${package_name}_${version}_${supported_arch}.deb"
-download_url="https://github.com/WOOD6563/WaifuDownloader-termux/releases/download/waifudownloader/${deb_file_name}"
+app_arch=$(uname -m)
+case "$app_arch" in
+aarch64) archtype="aarch64" ;;
+*) print_failed "Unsupported architectures" ;;
+esac
 
+deb_file_name="waifudownloader_${version}_${archtype}.deb"
+check_and_delete "$deb_file_name"
+download_file "https://github.com/WOOD6563/WaifuDownloader-termux/releases/download/WaifuDownloader/${deb_file_name}"
 dpkg --configure -a
 apt --fix-broken install -y
-apt install ./"$deb_file_name" -y
+apt install ./${deb_file_name} -y

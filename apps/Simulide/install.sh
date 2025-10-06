eb8231b5ae328d7a7dd67d19bda67e4da97ac773
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-06 22:20:18 +0530

    addpkg: SimulIDE

---
+++ apps/Simulide/install.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="simulide"
+run_cmd="simulide"
+version="termux_local_version"
+app_type="native"
+package_install_and_check "$package_name"

82a7780175bfffa3a4637853e312620f11f23c58
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-22 19:21:27 +0530

    addpkg: blender,blender4

--- apps/Blender4/install.sh
+++ apps/Blender4/install.sh
@@ -1,8 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 supported_arch="aarch64"
-package_name="blender"
-run_cmd="blender"
+package_name="blender4"
+run_cmd="blender-4.5"
 version="termux_local_version"
 app_type="native"
 package_install_and_check "$package_name"

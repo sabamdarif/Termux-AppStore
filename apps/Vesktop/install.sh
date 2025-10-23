85f3e1d03824e06e50883cd2e9e07f2263ba551c
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-10-23 18:35:29 +0530

    bump(Vesktop): v1.6.0

--- apps/Vesktop/install.sh
+++ apps/Vesktop/install.sh
@@ -3,7 +3,7 @@
 supported_arch="aarch64"
 package_name="vesktop"
 run_cmd="vesktop --no-sandbox"
-version="v1.5.4"
+version="v1.6.0"
 pause_update=true
 app_type="distro"
 supported_distro="all"
@@ -11,34 +11,34 @@ page_url="https://github.com/Vencord/Vesktop"
 working_dir="${distro_path}/root"
 
 if [[ "$selected_distro" == "ubuntu" ]] || [[ "$selected_distro" == "debian" ]]; then
-cd $working_dir
-filename="vesktop_${version#v}_arm64.deb"
-distro_run "
-check_and_delete "/root/${filename}"
+    cd "$working_dir" || return
+    filename="vesktop_${version#v}_arm64.deb"
+    distro_run "
+check_and_delete '/root/${filename}'
 "
-download_file "${page_url}/releases/download/${version}/${filename}"
-distro_run "
-sudo apt update -y -o Dpkg::Options::="--force-confnew"
+    download_file "${page_url}/releases/download/${version}/${filename}"
+    distro_run "
+sudo apt update -y -o Dpkg::Options::='--force-confnew'
 sudo apt install ./${filename} -y
-check_and_delete "/root/${filename}"
+check_and_delete '/root/${filename}'
 "
 elif [[ "$selected_distro" == "fedora" ]]; then
-cd $working_dir
-filename="vesktop_${version#v}_aarch64.rpm"
-distro_run "
-check_and_delete "/root/${filename}"
+    cd "$working_dir" || return
+    filename="vesktop_${version#v}_aarch64.rpm"
+    distro_run "
+check_and_delete '/root/${filename}'
 "
-download_file "${page_url}/releases/download/${version}/${filename}"
-distro_run "
+    download_file "${page_url}/releases/download/${version}/${filename}"
+    distro_run "
 sudo dnf install ./${filename} -y
-check_and_delete "/root/${filename}"
+check_and_delete '/root/${filename}'
 "
 else
     print_failed "Unsupported distro"
 fi
 
 print_success "Creating desktop entry..."
-cat <<DESKTOP_EOF | tee ${PREFIX}/share/applications/pd_added/vesktop.desktop >/dev/null
+cat <<DESKTOP_EOF | tee "${PREFIX}"/share/applications/pd_added/vesktop.desktop >/dev/null
 [Desktop Entry]
 Name=Vesktop
 Exec=pdrun ${run_cmd} --no-sandbox

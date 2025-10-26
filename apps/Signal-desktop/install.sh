ddac83f43f0a31cc4cdc2e265ea5b213fa7a2bb1
GitHub Action <github-actions[bot]@users.noreply.github.com>
2025-10-26 01:57:51 +0000

    Update app versions with latest releases from GitHub

--- apps/Signal-desktop/install.sh
+++ apps/Signal-desktop/install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 supported_arch="aarch64"
-version="v7.75.1"
+version="v7.76.0"
 app_type="distro"
 supported_distro="all"
 working_dir="${distro_path}/root"

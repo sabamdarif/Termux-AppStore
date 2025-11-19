25ae1a12f3e043814fff7da91c362e7fcb14edb8
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-11-19 17:26:33 +0530

    addpkg: luanti

---
+++ apps/Luanti/install.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="luanti"
+run_cmd="luanti"
+version="termux_local_version"
+app_type="native"
+package_install_and_check "$package_name"

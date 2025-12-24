0547cabc6c829cce8565ce29479bb2d2440ce3f5
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2025-12-24 13:24:44 +0530

    addpkg: audiotube

---
+++ apps/Audiotube/install.sh
@@ -0,0 +1,8 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="audiotube"
+run_cmd="audiotube"
+version="termux_local_version"
+app_type="native"
+package_install_and_check "$package_name kf6-kitemmodels"

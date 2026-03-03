1a873c28057b295ce394f6ffb72a1fbecc11755a
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-03-03 16:14:01 +0530

    addpkg: yt-dlp-gui

---
+++ apps/ytdlp-gui/uninstall.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+package_remove_and_check "yad"
+check_and_delete "$HOME/.local/bin/yt-dlp-gui"
+check_and_delete "$HOME/.config/ytdlp-gui/"
+check_and_delete "${PREFIX}/share/applications/ytdlp-gui.desktop"

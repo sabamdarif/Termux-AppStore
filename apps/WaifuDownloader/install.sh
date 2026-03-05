3e4fb4501dd8b703773d935338e522ec0242f3c0
Wood6563 <arparnasing@gmail.com>
2026-03-05 11:49:31 +0530

    Update waifudownloader (#16)

--- apps/WaifuDownloader/install.sh
+++ apps/WaifuDownloader/install.sh
@@ -3,7 +3,7 @@
 supported_arch="aarch64"
 package_name="waifudownloader"
 run_cmd="waifudownloader"
-version="0.2.9"
+version="0.2.69"
 app_type="native"
 
 app_arch=$(uname -m)
@@ -14,7 +14,7 @@ esac
 
 deb_file_name="waifudownloader_${version}_${archtype}.deb"
 check_and_delete "$deb_file_name"
-download_file "https://github.com/WOOD6563/WaifuDownloader-termux/releases/download/WaifuDownloader/${deb_file_name}"
+download_file "https://github.com/WOOD6563/WaifuDownloader-termux/releases/download/Master/${deb_file_name}"
 dpkg --configure -a
 apt --fix-broken install -y
 apt install ./${deb_file_name} -y

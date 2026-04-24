c507cb47b3ae6c2bba6ff0adcabe4510705b7f55
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-04-24 22:00:39 +0530

    feat: add option to build and release the appstore as a package

---
+++ appstore/termux-packages/build.sh
@@ -0,0 +1,21 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/sabamdarif/Termux-AppStore
+TERMUX_PKG_DESCRIPTION="A simple gtk3 appstore for Termux"
+TERMUX_PKG_LICENSE="GPL-3.0"
+TERMUX_PKG_MAINTAINER="@sabamdarif"
+TERMUX_PKG_VERSION="@VERSION@"
+TERMUX_PKG_SRCURL="git+https://github.com/sabamdarif/Termux-AppStore"
+TERMUX_PKG_DEPENDS="python, pygobject, python-pillow, gtk3, glib, aria2, gobject-introspection"
+TERMUX_PKG_BUILD_DEPENDS="pkg-config, xorgproto"
+TERMUX_PKG_PYTHON_RUNTIME_DEPS="fuzzysearch"
+
+termux_step_post_get_source() {
+	cd "$TERMUX_PKG_SRCDIR"
+	find . -mindepth 1 -maxdepth 1 ! -name 'appstore' ! -name '.git' -exec rm -rf {} +
+}
+
+termux_step_configure() {
+	TERMUX_PKG_SRCDIR="$TERMUX_PKG_SRCDIR/appstore"
+	TERMUX_PKG_BUILDDIR="$TERMUX_PKG_SRCDIR/build"
+	termux_setup_meson
+	termux_step_configure_meson
+}

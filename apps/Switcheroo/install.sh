51aea7bdbfeac7ae3be56c4e6ebcb96aae38e36b
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-05-06 20:28:34 +0530

    addpkg: switcheroo

---
+++ apps/Switcheroo/install.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="switcheroo"
+run_cmd="switcheroo"
+version="termux_local_version"
+app_type="native"
+progress_phase "prepare" 0 "Preparing to install..."
+package_install_and_check "$package_name"
+progress_done

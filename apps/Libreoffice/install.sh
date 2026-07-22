12ae6bdf60fa01a6c6414feddbb9d08ee3bf7db4
Md Arif <111168803+sabamdarif@users.noreply.github.com>
2026-07-22 18:04:29 +0530

    uppkg: libreoffice

---
+++ apps/Libreoffice/install.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+supported_arch="aarch64,arm"
+package_name="libreoffice"
+run_cmd="libreoffice"
+version="termux_local_version"
+app_type="native"
+
+package_install_and_check "$package_name"
---
+++ apps/Libreoffice/uninstall.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+app_type="native"
+
+standard_uninstall --native "libreoffice"
